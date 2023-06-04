534b30a28e6293b8158244662515d6c6dcc731b1
mims <113519976+MimPMC@users.noreply.github.com>
2023-06-04 20:47:35 +0200

    middleware

--- server/src/resources/users/userController.ts
+++ server/src/resources/users/userController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { z } from "zod";
 import { UserModel } from "./userModel";
 import { userRegisterSchema } from "./userValdation";
@@ -28,3 +28,21 @@ export async function registerUser(req: Request, res: Response) {
         res.status(500).send ("internal server error")
     }
 }
+
+
+export async function validationMiddleware(schema: z.Schema) {
+    return (req: Request, res: Response, next: NextFunction) => {
+      const inputData = req.body;
+      try {
+        schema.parse(inputData);
+        next(); // Validation successful, proceed to the next middleware or route handler
+      } catch (error) {
+        if (error instanceof z.ZodError) {
+          const errorMessage = error.errors[0]?.message || "Validation error";
+          res.status(400).send({ error: errorMessage });
+        } else {
+          res.status(400).send({ error: "Validation error" });
+        }
+      }
+    };
+  }
