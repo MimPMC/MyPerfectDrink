aa70faca5a4496662d612e56845830649dd7eab4
mims <113519976+MimPMC@users.noreply.github.com>
2023-06-04 20:24:56 +0200

    username zod validation

--- server/src/resources/users/userController.ts
+++ server/src/resources/users/userController.ts
@@ -1,9 +1,25 @@
 import { Request, Response } from "express";
+import { z } from "zod";
 import { UserModel } from "./userModel";
+import { userRegisterSchema } from "./userValdation";
 
 export async function registerUser(req: Request, res: Response) {
-    const { username, email, password } = req.body;
+    const inputData = req.body;
     try {
+        userRegisterSchema.parse(inputData)
+        
+    } catch (error) {
+        if(error instanceof z.ZodError) {
+            const errorMessage = error.errors[0]?.message || "Validation error";
+            res.status(400).send({ error: errorMessage });
+            return;
+        } else  {
+            res.status(400).send({error: "Validation error"})
+            return;
+        }
+    }
+    try {
+        const { username, email, password } = inputData
         const user = new UserModel({username, email, password})
         await user.save();
         res.status(201).send({message: `user${username} registered succefully!`})
@@ -11,5 +27,4 @@ export async function registerUser(req: Request, res: Response) {
         console.error(error)
         res.status(500).send ("internal server error")
     }
-    console.log("hello")
 }
--- server/src/resources/users/userModel.ts
+++ server/src/resources/users/userModel.ts
@@ -3,14 +3,14 @@ import { InferSchemaType, Schema, model } from "mongoose";
 
 export const userSchema = new Schema({
     username: {type: String, required:true, minlength:3},
-    email: { type: String, required: true, minlength: 3 },
+    email: { type: String, required: true, minlength: 5 },
     password: {
       type: String,
       required: true,
       minlength: 3,
     },
     isAdmin: { type: Boolean, default: false },
-  });
+  }, {timestamps: true});
   
   export type User = InferSchemaType<typeof userSchema>;
 
